2163d0fd26d6ec5b4cb61d2c9106813f39142835
krishipatel154 <krishipatel1542005@gmail.com>
2024-12-04 22:40:05 +0530

    added more files

---
+++ stringMethods.py
@@ -0,0 +1,10 @@
+name = "Krishi"
+surname = "Patel"
+print(name.upper())
+print(name.lower())
+print(surname.replace('tel','til'))
+print(name.find('i'))
+print('i' in name)
+# It doesn't modify the existing string
+print(name)
+print(surname)
