42c1321c3505c79236a0a05e59d0a4ea1b7a7200
krishipatel154 <krishipatel1542005@gmail.com>
2024-12-04 22:41:17 +0530

    update operators file

---
+++ operators.py
@@ -0,0 +1,37 @@
+# Arithmetic operators
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(num1+num2)
+print(num1-num2)
+print(num1/num2)
+print(num1//num2)
+print(num1*num2)
+print(num1**num2)
+print(num1%num2)
+
+# comparision operators
+print(num1>num2)
+print(num1>=num2)
+print(num1<num2)
+print(num1<=num2)
+print(num1==num2)
+print(num1!=num2)
+
+# Logical operator
+age = 20
+doReverse = (not age>18)
+isAligible = age>=18 and age<=35
+# isAligible = age>=18 or age<=35
+print(isAligible)
+print(doReverse)
+
+# assignment operator
+name = "Krishi"
+num1+= 20
+print(num1)
+
+# operator precedence
+# ()
+# /, *
+# +, -
+
