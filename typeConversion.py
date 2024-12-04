3604e49247753f8e6911c0b228902e0daf5bf3aa
krishipatel154 <krishipatel1542005@gmail.com>
2024-12-04 07:56:31 +0530

    added type conversion

---
+++ typeConversion.py
@@ -0,0 +1,6 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+add = num1+num2
+total = int(num1) + int(num2)
+print("Addition without type casting: "+add)
+print("Addition with type casting: : ",total)
