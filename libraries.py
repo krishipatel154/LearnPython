1733fed752f9502d283085c6ff93f2ac1a6af48e
krishipatel154 <krishipatel1542005@gmail.com>
2024-12-18 21:07:05 +0530

    new files added

---
+++ libraries.py
@@ -0,0 +1,9 @@
+# Libraries are collection of functions and methods that allow you to perform many actions without writing your code.
+
+# 1> NumPy: stands for Numerical Python and is the core library for numeric and sientific calculation.
+import  numpy as np
+n1 = np.array([1,2,3,4,5])
+print(n1)
+
+n2 = np.array([[1,2,3,4,5],[6,7,8,9,0]])
+print(n2)
